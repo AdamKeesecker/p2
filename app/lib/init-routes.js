9f21968145bd0c56ab76379fed29bc32405b1ec4
Adam Keesecker <adamkeesecker@gmail.com>
2014-06-06 14:56:32 -0500

    hardyharhar

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -15,8 +15,15 @@ module.exports = (req, res, next)=>{
 
 function load(app, fn){
   var home = traceur.require(__dirname + '/../routes/home.js');
+  var users = traceur.require(__dirname + '/../routes/users.js');
 
   app.get('/', dbg, home.index);
+  app.get('/users/dashboard', dbg, users.dashboard);
+
+
+
+
+
   console.log('Routes Loaded');
   fn();
 }
