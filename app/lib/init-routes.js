b98d4fcce21ee092c42d58c8a087045713c818db
Adam Keesecker <adamkeesecker@gmail.com>
2014-06-06 15:41:14 -0500

    k

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -20,7 +20,7 @@ function load(app, fn){
   app.get('/', dbg, home.index);
   app.post('/register', dbg, users.register);
   app.post('/login', dbg, users.login);
-  app.get('/users/dashboard/:id', dbg, users.loadDashboard);
+  app.get('/users/dashboard', dbg, users.loadDashboard);
 
 
 
